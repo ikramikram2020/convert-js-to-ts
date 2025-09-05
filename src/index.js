7719676f7b06615eeadf7c19c9a7b7653f869a2a
Meriem1551 <mm_boussaid@esi.dz>
2025-09-05 19:14:51 +0100

    adding js files - mini-project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const { addTodo, toggleTodo, getTodos } = require("./todo");
+
+// Example usage
+addTodo("Learn JavaScript");
+addTodo("Convert to TypeScript");
+
+console.log("All Todos:", getTodos());
+
+toggleTodo(getTodos()[0].id);
+
+console.log("After toggling:", getTodos());
---
+++ src/todo.js
@@ -0,0 +1,18 @@
+let todos = [];
+
+function addTodo(title) {
+  todos.push({ id: Date.now(), title, done: false });
+}
+
+function toggleTodo(id) {
+  todos = todos.map(todo =>
+    todo.id === id ? { ...todo, done: !todo.done } : todo
+  );
+}
+
+function getTodos() {
+  return todos;
+}
+
+// Export functions
+module.exports = { addTodo, toggleTodo, getTodos };
