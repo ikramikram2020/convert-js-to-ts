c808e8a009873c4026db78f1ecc53105488b6843
kawthar <kawtharikram2020@gmail.com>
2025-09-10 20:50:35 +0100

    chore: remove node_modules and dist from repo, add .gitignore

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const todo_1 = require("./todo");
-(0, todo_1.addTodo)("Learn JavaScript");
-(0, todo_1.addTodo)("Convert to TypeScript");
-console.log("All Todos:", (0, todo_1.getTodos)());
-(0, todo_1.toggleTodo)((0, todo_1.getTodos)()[0].id);
-console.log("After toggling:", (0, todo_1.getTodos)());
-//# sourceMappingURL=index.js.map
